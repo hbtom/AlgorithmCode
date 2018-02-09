475f91b10f33c5ef95323d779d3730cd748165fc
Wenqi.Wang@technicolor.com <wenqiwang@pal-nat186-32-169.itap.purdue.edu>
2018-02-08 22:30:09 -0500

    update

--- C++/Demo_BinarySearch.cpp
+++ C++/Demo_BinarySearch.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 // search x in a within range [left, right], return the index
-int BS(int a[], int x, int left, int right){
+int BS1(int a[], int x, int left, int right){
 	while(left < right){
 		int mid = left + (right-left)/2;
 		if(a[mid] < x){
@@ -15,9 +15,25 @@ int BS(int a[], int x, int left, int right){
 	return a[left]==x? left : -1;
 }
 
+int BS2(int a[], int x, int left, int right){
+	while(left <= right){
+		int mid = left + (right-left)/2;
+		if(a[mid] < x){
+			left = mid+1;}
+		else if(a[mid]>x){
+			right = mid -1;}
+		else
+			return mid;
+	}
+	return -1;
+}
+
 int main(){
-	int a[]={1,2,3,4,5,6,7,8,9};
-	int x = 5;
-	cout << BS(a, x,0, 8) << endl;
+	int a[]={1,2,3,4,5,6,7};
+	int x = 7;
+	int start = 0;
+	int end = 6;
+	cout << BS1(a, x,start, end) << endl;
+	cout << BS2(a, x,start, end) << endl;
 	return 1;
 }
