03a2171dfa07a1f0fa65634b9bb5984c34f9c59e
Wenqi.Wang@technicolor.com <wenqiwang@pal-nat186-32-169.itap.purdue.edu>
2018-02-08 19:57:20 -0500

    Python_Inheritance

---
+++ Python/Demo_Inheritance.py
@@ -0,0 +1,20 @@
+class Person:
+	def __init__(self, first, last):
+		self.firstname = first
+		self.lastname = last 
+
+	def Name(self):
+		return self.firstname + " " + self.lastname
+
+class Emplyee(Person):
+	def __init__(self, first, last, staffnum):
+		Person.__init__(self, first, last)
+		self.staffnumber = staffnum
+
+	def GetEmployee(self):
+		return self.Name() + " " + self.staffnumber
+
+x = Person("Marge", "Simpson")
+y = Emplyee("Homer", "Simpson", '1007')
+print(x.Name())
+print(y.GetEmployee())
